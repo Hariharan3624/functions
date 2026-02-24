93db674e7c54a8843642303e87ab016ea0c0636a
Hariharan3624 <vikrammanoharan@gmail.com>
2026-02-24 19:12:42 +0000

    Add files via upload

---
+++ shutdown.py
@@ -0,0 +1,8 @@
+a = (input("do you want to shoutdown your computer yes/no: "))
+if a == "yes":
+        print("shuting down")
+elif a == "no":
+    print("abort shutdown")
+else:
+    print("sorry, invalid input")
+    
